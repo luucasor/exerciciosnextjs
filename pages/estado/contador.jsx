d26fafe0163da6671d52822fdac77fbee3030364
luucasor <lucas@jornadatecnologia.com>
2023-04-01 12:08:48 -0300

    Desafio1 contador com estado

--- pages/estado/contador.jsx
+++ pages/estado/contador.jsx
@@ -2,12 +2,13 @@ import { useState } from "react"
 
 export default function contador() {
     const [contador, setContador] = useState(0)
-    
+
+    const dec = () => setContador(contador -1)
     return (
         <div>
             <h1>Contador</h1>
             <div>Valor: {contador}</div>
-            <button onClick={() =>  {setContador(contador -1)}}>-</button>
+            <button onClick={dec}>-</button>
             <button onClick={() =>  {setContador(contador +1)}}>+</button>
         </div>
     )
