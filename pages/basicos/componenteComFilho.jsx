15b1b8905faf83fb126d082ef452490704221e00
luucasor <lucas@jornadatecnologia.com>
2023-04-01 10:53:32 -0300

    Exemplo1 de reenderização condicionalcom children

---
+++ pages/basicos/componenteComFilho.jsx
@@ -0,0 +1,17 @@
+import Lista from "../../components/Lista";
+import Item from "../../components/Item";
+
+export default function componenteComFilho() {
+    return (
+        <div>
+            <Lista>
+                <Item conteudo="Item #1"/>
+                <Item conteudo="Item #2"/>
+                <Item conteudo="Item #3"/>
+                <Item conteudo="Item #11"/>
+                <Item conteudo="Item #12"/>
+                <Item conteudo="Item #13"/>
+            </Lista>
+        </div>
+    )
+}
