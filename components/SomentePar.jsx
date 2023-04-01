4dcb38bae67da0644f80038b8059846671845b06
luucasor <lucas@jornadatecnologia.com>
2023-04-01 10:42:27 -0300

    Exemplo1 de reenderização condicional

--- components/SomentePar.jsx
+++ components/SomentePar.jsx
@@ -1,5 +1,13 @@
 export default function SomentePar(props) {
     const numeroPar = props.numero % 2 === 0;
 
-    return numeroPar ? <span>{props.numero} </span> : null
+    //return numeroPar ? <span>{props.numero} </span> : null
+    return (
+        <div>
+            {numeroPar ?
+                <span>{props.numero}</span> :
+                null
+            }
+        </div>
+    )
 }
