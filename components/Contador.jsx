f761659a0acd80b9c5f4c714d22e1f87f87bee92
luucasor <lucas@jornadatecnologia.com>
2023-04-06 17:33:08 -0300

    Componente baseado em classe

---
+++ components/Contador.jsx
@@ -0,0 +1,58 @@
+import { Component } from "react";
+
+export default class Contador extends Component {
+
+    // constructor(props) {
+    //     super(props)
+
+    //     this.state = {
+    //         numero: props.valorInicial ?? 0,
+    //         passo: props.passo ?? 1
+    //     }
+    // }
+
+    state = {
+        numero: this.props.valorInicial ?? 0,
+        passo: this.props.passo ?? 1
+    }
+
+    inc = () => {
+        this.setState({
+            numero: this.state.numero + this.state.passo
+        })
+    }
+
+    dec = () => {
+        this.setState({
+            numero: this.state.numero - this.state.passo
+        })
+    }
+
+    alterarPasso = (ev) => {
+        this.setState({ passo: +ev.target.value })
+    }
+
+    renderForm() {
+        return (
+            <>
+                <input  type="number" 
+                        min={1} max={1000} 
+                        value={this.state.passo}
+                        onChange={this.alterarPasso}
+                />
+                <button onClick={this.dec}>-</button>
+                <button onClick={this.inc}>+</button>
+            </>
+        )
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>Contador (Usando classe)</h1>
+                <h2>{this.state.numero}</h2>
+                {this.renderForm()}
+            </div>
+        )
+    }
+}
